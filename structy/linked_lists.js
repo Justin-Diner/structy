e2e20604bb21762091338908e09a8ac4aa130eaa
Justin <90215009+DinerJ@users.noreply.github.com>
2023-06-10 22:40:18 -0400

    linkedlists

--- structy/linked_lists.js
+++ structy/linked_lists.js
@@ -437,3 +437,41 @@ const insertNodeRec = (head, value, index, count = 0) => {
   insertNode(head.next, value, index, count + 1);
   return head; 
 }
+
+// Create a LinkedList problem 
+const createLinkedList = (values) => {
+  const dummyHead = new Node(null);
+  let tail = dummyHead;
+
+  for (let val of values) {
+    tail.next = new Node(val);
+    tail = tail.next; 
+  }
+  return dummyHead.next; 
+}
+
+// This solution has O(n) time complexity and O(n) space complexity because you are creating O(1+n) nodes. 
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null);
+  let tail = dummyHead; 
+  
+  let carry = 0;
+  let current1 = head1; 
+  let current2 = head2;
+
+  
+  while (current1 != null || current2 != null || carry != 0) {
+    const val1 = current1 === null ? 0 : current1.val; 
+    const val2 = current2 === null ? 0 : current2.val; 
+    const sum = val1 + val2 + carry; 
+    carry = sum > 9 ? 1 : 0; 
+    
+    const digit = sum % 10;
+    tail.next = new Node(digit);
+    tail = tail.next; 
+    
+    if (current1 != null) current1 = current1.next; 
+    if (current2 != null) current2 = current2.next; 
+  }
+  return dummyHead.next; 
+};
