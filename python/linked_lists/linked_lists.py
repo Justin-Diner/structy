80a007adbe487988af6a8c0f23a2cd3924ce71dd
Justin <90215009+DinerJ@users.noreply.github.com>
2023-06-29 12:17:24 -0400

    reverse linked list

--- python/linked_lists/linked_lists.py
+++ python/linked_lists/linked_lists.py
@@ -36,3 +36,55 @@ def sum_list(head):
     return 0
   return head.val + sum_list(head.next)
   
+# linked list find (iterative)
+def linked_list_find(head, target):
+  current = head
+  
+  while current is not None:
+    if current.val == target:
+      return True 
+    current = current.next
+  
+  return False
+
+# linked list find (recursive)
+def linked_list_find(head, target):
+  if head is None:
+    return False
+  if head.val == target:
+    return True
+  return linked_list_find(head.next, target)
+
+# get node value (iterative)
+def get_node_value(head, index):
+  count = 0
+  current = head
+  while current is not None:
+    if count == index:
+      return current.val
+    
+    current = current.next
+    count += 1
+    
+  return None
+
+# get node value(recursive)
+def get_node_value(head, index):
+  if head is None:
+    return None
+  if index == 0:
+    return head.val
+  return get_node_value(head.next, index - 1)
+
+# reverse linked list (iterative)
+def reverse_list(head):
+  prev = None
+  current = head
+  
+  while current is not None:
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+  
+  return prev 
