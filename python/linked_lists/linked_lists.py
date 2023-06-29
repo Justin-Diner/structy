41cafe8c289fe0dc288a8654f2998780b3ee3a88
Justin <90215009+DinerJ@users.noreply.github.com>
2023-06-29 11:39:39 -0400

    sum list

--- python/linked_lists/linked_lists.py
+++ python/linked_lists/linked_lists.py
@@ -8,9 +8,31 @@ def linked_list_values(head):
     current = current.next
   return values
   
-# recursive
+# recursive 
 def linked_list_values(head):  
-  if head == None:
-    return []
+  values = []
+  fill_values(head, values)
+  return values
+
+def fill_values(head, values):
+  if head is None:
+    return
+  values.append(head.val)
+  fill_values(head.next, values)
   
-  return [head.val] + linked_list_values(head.next) 
+# sum_list iterative
+def sum_list(head):
+  total_sum = 0
+  current = head
+  while current is not None:
+    total_sum += current.val
+    current = current.next
+  return total_sum
+  
+
+# sum_list recursive
+def sum_list(head):
+  if head is None: 
+    return 0
+  return head.val + sum_list(head.next)
+  
