fa5e580e0ea132268336d09f42b3ede94db488d7
Justin <90215009+DinerJ@users.noreply.github.com>
2023-07-25 16:02:45 -0400

    minimum island in python

--- python/graphs/graphs.py
+++ python/graphs/graphs.py
@@ -225,3 +225,48 @@ def exploreIsland(grid, r, c, visited):
   exploreIsland(grid, r, c-1, visited)
   exploreIsland(grid, r, c+1, visited)
   return True
+
+def minimum_island(grid):
+    minimum = float('inf')
+    visited = set()
+    
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            size = find_size(grid, r, c, visited)
+            if size > 0 and size < minimum:
+                minimum = size
+    
+    return minimum
+
+def find_size(grid, r, c, visited):
+    inRow = 0 <= r and r < len(grid)
+    inCol = 0 <= c and c < len(grid[0])
+    if (inRow == False or inCol == False):
+        return 0
+    
+    if grid[r][c] == 'W':
+        return 0
+    
+    pos = (r, c)
+    if pos in visited:
+        return 0
+    visited.add(pos)
+    
+    size = 1
+    size += find_size(grid, r - 1, c, visited)
+    size += find_size(grid, r + 1, c, visited)
+    size += find_size(grid, r, c - 1, visited)
+    size += find_size(grid, r, c + 1, visited)
+    
+    return size
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+
+print(minimum_island(grid))
