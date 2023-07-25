7dd437a908b1cfd3a655dfdc8b4017d856851b89
Justin <90215009+DinerJ@users.noreply.github.com>
2023-07-25 14:07:24 -0400

    explore islands js

--- structy/graphs.js
+++ structy/graphs.js
@@ -197,3 +197,37 @@ const shortestPath = (edges, nodeA, nodeB) => {
     
     return final
   }
+
+const islandCount = (grid) => {
+let islands = 0;
+const visited = new Set();
+
+for (let r = 0; r < grid.length; r += 1) {
+    for (let c = 0; c < grid[0].length; c+= 1) {
+    if (exploreIsl(grid, r, c, visited) === true) {
+        islands += 1;
+    }
+    }
+}
+return islands;
+};
+  
+const exploreIsl = (grid, r, c, visited) => {
+    const rowInbounds = 0 <= r && r < grid.length;
+    const colInbounds = 0 <= c && c < grid[0].length; 
+
+    if (!rowInbounds || !colInbounds) return false; 
+
+    if (grid[r][c] === 'W') return false;
+
+    const pos = r + ',' + c;
+    if (visited.has(pos)) return false; 
+    visited.add(pos);
+
+    explore(grid, r-1, c, visited);
+    explore(grid, r+1, c, visited);
+    explore(grid, r, c-1, visited);
+    explore(grid, r, c+1, visited);
+
+    return true; 
+}
