9776e943255651fdbdf9f90aabd5babc6b92d671
Justin <90215009+DinerJ@users.noreply.github.com>
2023-08-25 15:35:04 -0400

    determineSems

--- structy/graphs.js
+++ structy/graphs.js
@@ -315,3 +315,47 @@ const closestCarrot = (grid, startRow, startCol) => {
   let pos = [startRow, startCol]
   const queue = [ pos ]    
 }
+
+const semestersRequired = (numCourses, prereqs) => {
+  const semGraph = makeGraph(numCourses, prereqs);
+  const distance = {}; 
+  
+  for (let i = 0; i < numCourses; i+=1) {
+    if (semGraph[i].length === 0) distance[i] = 1;
+  }
+  
+  for (let i = 0; i < numCourses; i += 1) {
+    determineSems(semGraph, i, distance)
+  }
+  
+  return Math.max(...Object.values(distance))
+};
+
+const determineSems = (graph, node, distance) => {
+  if (node in distance) return distance[node];
+  
+  let maxDistance = 0; 
+  
+  for (let neighbor of graph[node]) {
+    const neighborDistance = determineSems(graph, neighbor, distance);
+    if (neighborDistance > maxDistance) maxDistance = neighborDistance
+  }
+  
+  distance[node] = maxDistance + 1; 
+  return distance[node];
+}
+
+const makeGraph = (numCourses, prereqs) => {
+  const graph = {};
+  
+  for (let i = 0; i < numCourses; i +=1) {
+    graph[i] = [];
+  }
+  
+  for (let prereq of prereqs) {
+    const [a, b] = prereq; 
+    graph[a].push(b);
+  }
+  
+  return graph;
+}
